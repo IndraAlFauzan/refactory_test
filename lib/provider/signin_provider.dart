1df4a6285710b38fa0a0652e9f94309ce7735ca1
IndraAlFauzan <39118928+IndraAlFauzan@users.noreply.github.com>
2021-08-18 21:12:55 +0700

    service google signin

--- lib/provider/signin_provider.dart
+++ lib/provider/signin_provider.dart
@@ -1,7 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class SigninProvider extends ChangeNotifier {
   final googleSignin = GoogleSignIn();
-  
+
+  GoogleSignInAccount? user;
+
+  GoogleSignInAccount get getUser => user!;
+
+  Future googleLogin() async {
+    final googleUser = await googleSignin.signIn();
+
+    if (googleUser == null) return;
+    user = googleUser;
+
+    final googleAuth = await googleUser.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    await FirebaseAuth.instance.signInWithCredential(credential);
+
+    notifyListeners();
+  }
 }
